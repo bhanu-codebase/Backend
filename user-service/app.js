88fb1844f0e049bac3030553524ec5c26eb187d6
Bhanu <bhanupriyagupta672@gmail.com>
2025-06-01 23:43:49 +0530

    msg

--- user-service/app.js
+++ user-service/app.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/order', require('./routes/orderRoutes'));
+app.use('/order', require('./routes/OrderRoutes'));
 app.use('/rating', require('./routes/ratingRoutes'));
 app.use('/restaurants', require('./routes/restaurantRoutes'));
 app.use('/users', require('./routes/userRoutes'));
